93c8775c7424a2a47fc78236cad1e8ca84a68415
a17789712 <35755896+a17789712@users.noreply.github.com>
2019-03-24 20:56:50 +0800

    Add files via upload

--- src/pages/test/test.jsx
+++ src/pages/test/test.jsx
@@ -1,1 +1,19 @@
+import React, { Component } from 'react';
+import PublicHeader from '@/components/header/header';
+import { is, fromJS } from 'immutable';
+import './test.less';
 
+export default class HelpCenter extends Component {
+
+  shouldComponentUpdate(nextProps, nextState){
+    return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state), fromJS(nextState))
+  }
+
+  render(){
+    return (
+        <test>
+          <PublicHeader title="test" record/>
+        </test>
+    )
+  }
+}
